30092d31928b5a07a568edcd742a4257889d7a1c
GDNBHARATH448 <gollapallibharath24@gmail.com>
2025-10-07 19:39:13 +0530

    Update config.py

--- Biolink/config.py
+++ Biolink/config.py
@@ -3,8 +3,8 @@ from dotenv import load_dotenv
 
 load_dotenv()
 
-API_ID = int(os.getenv("API_ID", "29896633"))
-API_HASH = os.getenv("API_HASH", "7a8a6dd1c08f6ffc33645885bb3ecf77")
-BOT_TOKEN = os.getenv("BOT_TOKEN", "7697479557:AAGSYiTMd4biFnLw_rfJ-heLgIQYIOn53VQ")
-OWNER_ID = int(os.getenv("OWNER_ID", "5867783630"))
+API_ID = int(os.getenv("API_ID", "26715173"))
+API_HASH = os.getenv("API_HASH", "e208f4ac326676f273054f2095211fea")
+BOT_TOKEN = os.getenv("BOT_TOKEN", "7697479557:AAG2pYNsXfbM_SqOT2BqSMBodmaYmnhVEl4")
+OWNER_ID = int(os.getenv("OWNER_ID", "7946875913"))
 MONGO_URI = os.getenv("MONGO_URI", "mongodb+srv://Likhith:Likhithbots@likhith.jm3wayg.mongodb.net/?retryWrites=true&w=majority&appName=likhith")
